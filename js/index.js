3fb1ca8d84adf1f67a3a1b39449a88bdf4d01a33
ayydeji <abdulmkamal@hotmail.com>
2020-06-13 08:05:41 +0100

    updated projects section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
-$(window).on("load",function(){$(".hello-div").addClass("typewriter"),setTimeout(function(){setTimeout(function(){$(".hello-div").removeClass("typewriter"),$(".hello").addClass("visible"),$(".world-div").addClass("typewriter")},1e3)},500)}),$.fn.isInViewport=function(){var i=$(this).offset().top,e=i+$(this).outerHeight(),o=$(window).scrollTop(),t=o+$(window).height();return e>o&&i<t},$(window).on("resize scroll",function(){$(".about-me").each(function(){$(this).isInViewport()&&$(this).fadeIn(1500)})});
+$(window).on("load", function () {
+    $(".hello-div").addClass("typewriter"),
+        setTimeout(function () {
+            setTimeout(function () {
+                $(".hello-div").removeClass("typewriter"), $(".hello").addClass("visible"), $(".world-div").addClass("typewriter");
+            }, 1e3);
+        }, 500);
+}),
+    ($.fn.isInViewport = function () {
+        var i = $(this).offset().top,
+            e = i + $(this).outerHeight(),
+            o = $(window).scrollTop(),
+            t = o + $(window).height();
+        return e > o && i < t;
+    }),
+    $(window).on("scroll resize", function () {
+        $(".about-me").each(function () {
+            $(this).isInViewport() && $(this).fadeIn(1500);
+        });
+    });
+
+$(".more-button").on("click", function(){
+  $(".proj-more").toggleClass("invisible");
+  $(this).css("background-color", "#412025");
+  if ($(".proj-more").hasClass("invisible")){
+    $(".more-button").html("More Projects");
+  }else{
+    $(".more-button").html("Less Projects");
+  }
+});
